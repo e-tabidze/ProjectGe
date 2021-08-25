6b6a6a0781d5fce61d0798a660d5ec41ec37350c
Elene Tabidze <etabidze@outlook.com>
2021-08-25 23:18:22 +0400

    Changed API Endpoints

--- client/src/Services/APIEndpoints.jsx
+++ client/src/Services/APIEndpoints.jsx
@@ -1,14 +1,14 @@
 import http from "./httpService";
 
 export function getJewels() {
-  return http.get("http://localhost:3000/api/jewels/").then((res) => {
+  return http.get("/api/jewels/").then((res) => {
     return res.data;
   });
 }
 
 export function getSimilarJewels(type) {
   return http
-    .post("http://localhost:3000/api/jewels/similar", { type: type })
+    .post("/api/jewels/similar", { type: type })
     .then((res) => {
       console.log(res.data);
       return res.data;
@@ -16,62 +16,62 @@ export function getSimilarJewels(type) {
 }
 
 // export function getSuperJewels() {
-//   return http.get("http://localhost:3000/api/jewels/super").then((res) => {
+//   return http.get("/api/jewels/super").then((res) => {
 //     return res.data;
 //   });
 // }
 
 // export function getVipJewels() {
-//   return http.get("http://localhost:3000/api/jewels/vip").then((res) => {
+//   return http.get("/api/jewels/vip").then((res) => {
 //     return res.data;
 //   });
 // }
 
 // export function getDefaultJewels() {
-//   return http.get("http://localhost:3000/api/jewels/default").then((res) => {
+//   return http.get("/api/jewels/default").then((res) => {
 //     return res.data;
 //   });
 // }
 
 export function getJewel(id) {
   return http
-    .put(`http://localhost:3000/api/jewels/jewel`, { id: id })
+    .put(`/api/jewels/jewel`, { id: id })
     .then((res) => {
       return res.data;
     });
 }
 
 export function getStones() {
-  return http.get("http://localhost:3000/api/stones/").then((res) => {
+  return http.get("/api/stones/").then((res) => {
     return res.data;
   });
 }
 
 export function getMetals() {
-  return http.get("http://localhost:3000/api/metals/").then((res) => {
+  return http.get("/api/metals/").then((res) => {
     return res.data;
   });
 }
 
 export function getPieces() {
-  return http.get("http://localhost:3000/api/pieces/").then((res) => {
+  return http.get("/api/pieces/").then((res) => {
     return res.data;
   });
 }
 
 // export function getTypes() {
-//   return http.get("http://localhost:3000/api/types/").then((res) => {
+//   return http.get("/api/types/").then((res) => {
 //     return res.data;
 //   });
 // }
 
 export function getUserJewels(userId) {
-  return http.post("http://localhost:3000/api/users/jewels", { userId });
+  return http.post("/api/users/jewels", { userId });
 }
 
 export function postJewels(newJewel, userToken) {
   return http
-    .post("http://localhost:3000/api/jewels/add", newJewel, {
+    .post("/api/jewels/add", newJewel, {
       headers: {
         "x-auth-token": userToken,
         action: "/multiple-upload",
@@ -85,7 +85,7 @@ export function postJewels(newJewel, userToken) {
 }
 
 export async function editJewel(updatedJewel, jewelId, userToken) {
-  return await http.post(`http://localhost:3000/api/jewels/update/${jewelId}`, updatedJewel, {
+  return await http.post(`/api/jewels/update/${jewelId}`, updatedJewel, {
     headers: {
       "x-auth-token": userToken,
       action: "/multiple-upload",
@@ -102,7 +102,7 @@ export async function editJewel(updatedJewel, jewelId, userToken) {
 export function deleteJewel(jewelId, userToken) {
   console.log(jewelId, userToken, "jewelId nd Usertoken");
   return http
-    .delete(`http://localhost:3000/api/jewels/delete/${jewelId}`, {
+    .delete(`/api/jewels/delete/${jewelId}`, {
       headers: {
         "x-auth-token": userToken,
       },
@@ -113,18 +113,18 @@ export function deleteJewel(jewelId, userToken) {
 }
 
 export function registerUser(user) {
-  return http.post("http://localhost:3000/api/users/", user).then((res) => {
+  return http.post("/api/users/", user).then((res) => {
     return res;
   });
 }
 
 export function login(email, password) {
-  return http.post("http://localhost:3000/api/auth/", { email, password });
+  return http.post("/api/auth/", { email, password });
 }
 
 export function forgotPassword(email) {
   return http
-    .post("http://localhost:3000/api/password-reset", { email })
+    .post("/api/password-reset", { email })
     .then((res) => {
       return res.data;
     });
@@ -132,7 +132,7 @@ export function forgotPassword(email) {
 
 export function changePassword(userId, userToken, password) {
   return http
-    .post(`http://localhost:3000/api/password-reset/${userId}/${userToken}`, {
+    .post(`/api/password-reset/${userId}/${userToken}`, {
       password,
     })
     .then((res) => {
