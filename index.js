c77e828a6ad09485e0304bb2573d1ccf1a624af8
Elene Tabidze <etabidze@outlook.com>
2021-09-12 20:02:26 +0400

    Removed App.get

--- index.js
+++ index.js
@@ -98,9 +98,9 @@ if (process.env.NODE_ENV === "production") {
   // app.use(express.static(path.join(__dirname, "client", "build")));
   app.use(express.static("client/build"));
 
-  app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirnmame, "client", "build", "index.html"));
-  });
+  // app.get("*", function (req, res) {
+  //   res.sendFile(path.resolve(__dirnmame, "client", "build", "index.html"));
+  // });
 }
 
 const port = process.env.PORT || 3000;
