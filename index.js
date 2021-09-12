146015cb419f3c16646a15159ca42460b7954c9c
Elene Tabidze <etabidze@outlook.com>
2021-09-12 18:44:25 +0400

    Working on Routing

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ if (process.env.NODE_ENV === "production") {
   // app.use(express.static("client/build"));
 
   app.get("*", function (req, res) {
-    res.sendFile(path.join("client/build", "index.html"));
+    res.sendFile(path.join("/client/build", "index.html"));
   });
 }
 
